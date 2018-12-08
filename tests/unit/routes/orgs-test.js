f14b6d871cc17184864a38e18da1421b89f84d1e
industriasarse <industriasarse@gmail.com>
2018-12-08 18:24:48 +0100

    Routes exercise part 1
    - we've made first exercise about routes

--- tests/unit/routes/orgs-test.js
+++ tests/unit/routes/orgs-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | home/juan', function(hooks) {
+module('Unit | Route | orgs', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:home/juan');
+    let route = this.owner.lookup('route:orgs');
     assert.ok(route);
   });
 });
