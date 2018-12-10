5b6578429cddb6dd542d5d93fad05a6d4d5e4f9a
industriasarse <industriasarse@gmail.com>
2018-12-10 19:37:54 +0100

    Creating a list of favorite orgs

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+
+  favorites: service('favorites'),
+
+  actions: {
+    favoriteClicked(org) {
+      this.get('favorites').favoriteItem(org)
+    }
+  },
+
   model() {
     return [
       {id: "google"},
