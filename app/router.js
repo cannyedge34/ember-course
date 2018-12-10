25b330522436c5e9662763d00e81464611d53aa4
industriasarse <industriasarse@gmail.com>
2018-12-10 18:57:13 +0100

    Add elements to list
    1º We are adding elements to repo list

--- app/router.js
+++ app/router.js
@@ -7,15 +7,22 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  // LIST OF COMPANIES
   this.route('orgs'); // /orgs
+  
+  // INDIVIDUAL ORG
   this.route('org', {path: 'org/:id'}, function() { // /org/facebook
+    
+    //LIST OF REPOS
     this.route('repos');
+
+    // INDIVIDUAL REPO
     this.route('repo', {path: ':repoid'}, function() {
       this.route('issues');
       this.route('contributors');
     }); // org/facebook/react
   });
-  this.route('notfound', {path: '*path'})
+  this.route('notfound', {path: '*path'});
 });
 
 export default Router;
